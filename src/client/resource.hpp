d27d95c0d7940663c5b630ecf8acff8943fe9b47
Skull <86374920+skkuull@users.noreply.github.com>
2025-03-22 09:21:52 +0300

    feat(dw): upload dw publisher files

--- src/client/resource.hpp
+++ src/client/resource.hpp
@@ -5,13 +5,15 @@
 #define IMAGE_SPLASH          300
 #define IMAGE_LOGO            301
 
-#define DW_MOTD               302
-#define DW_PLAYLISTS          303
+#define DW_METAPLAYERDATA     302
+#define DW_MOTD               303
+#define DW_PLAYLISTS          304
+#define DW_SURVEYS            305
 
-#define MENU_MAIN             304
+#define MENU_MAIN             306
 
-#define DVAR_LIST             305
+#define DVAR_LIST             307
 
-#define TLS_DLL               306
+#define TLS_DLL               308
 
-#define ICON_IMAGE            307
+#define ICON_IMAGE            309
